c94cff3093d941246b367de9067ba1624e0126fd
Joshua Alvarado <joshalva3@gmail.com>
2024-01-10 23:51:52 -0600

    understaring default parameters

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const bookings = [];
+
+// setting default parameters of numPassengers and Price
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+// cannot skip number of passengers
+// second arguement will always be mapped as number of passenger
+// can set it to undefined to overlay it
+// ex: createBooking(`LH123`, undefined, 1000);
+
+createBooking(`LH123`);
+createBooking(`LH123`, 2, 800);
+createBooking(`LH123`, 2);
