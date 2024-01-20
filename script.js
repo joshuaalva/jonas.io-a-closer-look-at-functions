937b27a25193fcca1634667dc463e90c91041b82
Joshua Alvarado <joshalva3@gmail.com>
2024-01-19 20:39:42 -0600

    coding challenge #1 lecture and break down go back and review to understand the concepts better

--- script.js
+++ script.js
@@ -258,3 +258,27 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 
 // Coding Challenge #1
+const poll = {
+  question: `What is your faborite programming language?`,
+  options: [`0: JavaScript`, `1: Python`, `2: Rust`, `3: C++`],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get Answer
+    const answer = Number(
+      prompt(
+        `${this.question} \n ${this.options.join(`\n`)}\n(Write option number)`
+      )
+    );
+    // console.log(answer);
+    // Register answer
+    typeof answer === `number` &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    console.log(this.answers);
+  },
+};
+
+// poll.registerNewAnswer();
+document
+  .querySelector(`.poll`)
+  .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
